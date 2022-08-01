d73a60ce8a6ce11423350ff8f601e3ec66c0f51e
Ashwaganha-cader <max.cacz@yandex.ru>
2022-08-01 18:52:47 +0500

    done base glide

--- app/src/main/java/com/example/example_sample_glide/MainActivity.java
+++ app/src/main/java/com/example/example_sample_glide/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.example_sample_glide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+
+        Context context = MainActivity.this;
+
+        Glide.with(context)
+                .load(com.bumptech.glide.R.drawable.abc_ic_star_black_16dp)
+                .into(imageView);
+
+
     }
 }
